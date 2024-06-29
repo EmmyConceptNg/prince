a9b40b3d99a4cc5d5d0e2f516f929324270b56f7
Emmy <emmyconceptng@gmail.com>
2024-06-29 22:08:58 +0100

    auth

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -89,7 +89,7 @@ navigate("/dashboard");
   }, [googleUser]);
 
   return (
-    <Box bgcolor="" maxHeight="100vh">
+    <Box height="100vh" ml={{ lg: 20, xs: 0 }}>
       <ToastContainer />
       <Grid container spacing={1} justifyContent="space-between">
         <Grid item md={12} lg={7} xs={12} sm={12}>
