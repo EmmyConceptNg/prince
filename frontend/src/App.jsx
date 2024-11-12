8700c2bd32fa58b42cec62c21f3acdc938413b0e
Emmy <emmyconceptng@gmail.com>
2024-11-12 02:58:44 +0100

    fixed flickering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -76,7 +76,7 @@ function App() {
 
     // if (!localStorage.getItem("sessionValidated"))
     validateTokenOnce();
-  }, [sessionValidated, location.pathname, navigate]);
+  }, [sessionValidated, location.pathname]);
 
   return (
     <>
